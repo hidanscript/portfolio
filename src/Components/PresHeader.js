c7914f27b8147b296b1e2ff777ea1a2f13c197cb
hidanshira <hidanshira2@gmail.com>
2019-05-12 01:22:18 -0300

    Fixed Overflow.

--- src/Components/PresHeader.js
+++ src/Components/PresHeader.js
@@ -6,7 +6,7 @@ class PresHeader extends Component {
 	render() {
 		return(
 			<div>
-				<h1>Hello I'm <strong id="myName">David Ortiz</strong>.</h1>
+				<h1>Hello, I'm <strong id="myName">David Ortiz</strong>.</h1>
 				<p>Frontend Developer</p>
 				<button type="submit">Get Started</button>
 			</div>
